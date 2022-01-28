ca2129604fece632720ad7e74ffd270e3ff3dd9a
Edwin Zamora Oros <44784048+EdwinZamora@users.noreply.github.com>
2022-01-28 14:26:33 -0600

    Update BBB_LED_Blink.py

--- BBB_LED_Blink.py
+++ BBB_LED_Blink.py
@@ -1,10 +1,15 @@
-/*This assumes you have installed Adafruit_BBIO, if not please read de readme file*/
+#This assumes you have installed Adafruit_BBIO, if not please read de readme file
 
 
-import Adafruit_BBIO.GPIO as GPIO
+import Adafruit_BBIO.GPIO as GPIO #import GPIO library
 from time import sleep
-OutputLED = "P9_12"
+
+config-pin P9_14 gpio #Configures pin to GPIO
+
+OutputLED = "P9_12" #Select GPIO where you going to put the LED
+#setup the pin as OUTPUT
 GPIO.setup(OutputLED, GPIO.OUT)
+#Start the cycle to blink LED
 for i in range(0,5):
   GPIO.output(OutputLED, GPIO.HIGH)  
   sleep(5)
